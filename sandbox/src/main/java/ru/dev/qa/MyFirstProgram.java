387ce1da8f9e7a3a7a627dd77b2b54bef5ec470c
SomeVoices <89222561+SomeVoices@users.noreply.github.com>
2021-08-25 01:45:49 +0300

    added new methods

--- sandbox/src/main/java/ru/dev/qa/MyFirstProgram.java
+++ sandbox/src/main/java/ru/dev/qa/MyFirstProgram.java
@@ -1,8 +1,28 @@
 package ru.dev.qa;
 
 public class MyFirstProgram {
-	public static void main (String[] args){
+	public static void main(String[] args) {
 
-		System.out.println("Hello, world!");
+		hello("world");
+		hello("Lera");
+
+		double len = 2.;
+		System.out.println("Площадь квдарата со стороной " + len + " = " + area(len));
+
+		double a = 2.;
+		double b = 3.;
+		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+	}
+
+	public static void hello(String who){
+		System.out.println("Hello, " + who + "!");
+	}
+
+	public static double area(double len){
+		return len*len;
+	}
+
+	public static double area(double a, double b){
+		return a*b;
 	}
 }
