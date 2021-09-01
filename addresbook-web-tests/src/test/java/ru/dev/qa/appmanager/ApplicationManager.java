eb3868d5936f01fe10eb1dee86e56498a35977a2
SomeVoices <89222561+SomeVoices@users.noreply.github.com>
2021-09-01 21:02:10 +0300

    Created HelperBase

--- addresbook-web-tests/src/test/java/ru/dev/qa/appmanager/ApplicationManager.java
+++ addresbook-web-tests/src/test/java/ru/dev/qa/appmanager/ApplicationManager.java
@@ -29,31 +29,4 @@ public class ApplicationManager {
     wd.quit();
   }
 
-
-
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
-
-  public GroupHelper getGroupHelper() {
-    return groupHelper;
-  }
-
-  public NavigationHelper getNavigationHelper() {
-    return navigationHelper;
-  }
 }
